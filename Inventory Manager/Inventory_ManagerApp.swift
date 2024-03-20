70e7e3abf3c819b195ccea69c7cbf956b3af5189
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2024-03-20 19:37:32 +0530

    Initial Commit

---
+++ Inventory Manager/Inventory_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory_ManagerApp.swift
+//  Inventory Manager
+//
+//  Created by Harsh Bhikadiya on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Inventory_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
